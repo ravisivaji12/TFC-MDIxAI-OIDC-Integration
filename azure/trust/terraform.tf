dee8b263e94b7c8f7cade40bf235522c5ceca3b9
Ravi, Sivaji <ravi.sivaji@outlook.com>
2025-03-18 16:57:52 -0400

    Initial code files

---
+++ azure/trust/terraform.tf
@@ -0,0 +1,27 @@
+terraform {
+
+  cloud {
+    organization = "Mccain_Foods"
+
+    workspaces {
+      name = "Development"
+    }
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.34.1"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
---
+++ azure/trust/variables.tf
@@ -0,0 +1,29 @@
+variable "tfc_azure_audience" {
+  type        = string
+  default     = "api://AzureADTokenExchange"
+  description = "The audience value to use in run identity tokens"
+}
+
+variable "tfc_hostname" {
+  type        = string
+  default     = "app.terraform.io"
+  description = "The hostname of the TFC or TFE instance you'd like to use with Azure"
+}
+
+variable "tfc_organization_name" {
+  type        = string
+  default     = "Mccain_Foods"
+  description = "The name of your Terraform Cloud organization"
+}
+
+variable "tfc_project_name" {
+  type        = string
+  default     = "TFC-MDIxAI-OIDC-Integration"
+  description = "The project under which a workspace will be created"
+}
+
+variable "tfc_workspace_name" {
+  type        = string
+  default     = "Development"
+  description = "The name of the workspace that you'd like to create and connect to Azure"
+}
